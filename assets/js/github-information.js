b744f283168ade891e60e69bb10d1b7202fa2503
Fiona-T <fitracey@gmail.com>
2021-09-20 13:11:31 +0000

    feat: retrieve info from github api and render it, add styles

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,3 +1,22 @@
+
+// user is the object being returned from GitHub API
+function userInformationHTML(user) {
+    console.log(user);
+    // display username with link to their profile on github, avatar, following/ers, repos
+    return `
+    <h2>${user.name}<span class="small-name">(@<a href="${user.html_url}" target="_blank">${user.login}</a>)</span></h2>
+    <div class="gh-content">
+        <div class="gh-avatar">
+            <a href="${user.html_url}" target="_blank">
+                <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}">
+            </a>
+        </div>
+        <p>Followers: ${user.followers} - Following: ${user.following} <br>
+        Repos: ${user.public_repos}</p>
+    </div>`;
+
+}
+
 function fetchGitHubInformation(event) {
     // jQuery get value in gh-uername field
     let username = $("#gh-username").val();
@@ -13,4 +32,19 @@ function fetchGitHubInformation(event) {
         </div>`
     );
     
+    // handle response from github API, pass userData to userInformationHTML, or display error
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`)
+        ).then(
+            function(response) {
+                let userData = response;
+                $("#gh-user-data").html(userInformationHTML(userData));
+            }, function(errorResponse) {
+                if (errorResponse.status === 404) {
+                    $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
+                } else {
+                    console.log(errorResponse);
+                    $("#gh-user-data").html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+                }
+            });
 }
